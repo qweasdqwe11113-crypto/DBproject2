0585ec3f4cd990e3e82fb9830e8102fc986ec3c1
houjiaqing <qweasdqwe11113@gmail.com>
2026-05-07 16:28:13 +0800

    explain(简洁版)功能增加

--- engine-project-master/src/main/java/edu/sustech/cs307/logicalOperator/ddl/ExplainExecutor.java
+++ engine-project-master/src/main/java/edu/sustech/cs307/logicalOperator/ddl/ExplainExecutor.java
@@ -1,9 +1,14 @@
 package edu.sustech.cs307.logicalOperator.ddl;
 
+import edu.sustech.cs307.logicalOperator.LogicalOperator;
+import edu.sustech.cs307.optimizer.LogicalPlanner;
+import edu.sustech.cs307.optimizer.PhysicalPlanner;
+import edu.sustech.cs307.physicalOperator.PhysicalOperator;
 import edu.sustech.cs307.system.DBManager;
 import edu.sustech.cs307.exception.DBException;
 
 import net.sf.jsqlparser.statement.ExplainStatement;
+import org.pmw.tinylog.Logger;
 
 public class ExplainExecutor implements DMLExecutor {
 
@@ -17,6 +22,19 @@ public class ExplainExecutor implements DMLExecutor {
 
     @Override
     public void execute() throws DBException {
-       //todo: finish this function here, and add log info
+        String statementSql = explainStatement.getStatement().toString();
+        LogicalOperator logicalOperator = LogicalPlanner.resolveAndPlan(dbManager, statementSql);
+        if (logicalOperator == null) {
+            Logger.info("EXPLAIN: nothing to explain for statement: {}", statementSql);
+            return;
+        }
+
+        Logger.info("EXPLAIN Logical Plan:");
+        for (String line : logicalOperator.toString().split("\\R")) {
+            Logger.info(line);
+        }
+
+        PhysicalOperator physicalOperator = PhysicalPlanner.generateOperator(dbManager, logicalOperator);
+        Logger.info("EXPLAIN Physical Plan Root: {}", physicalOperator.getClass().getSimpleName());
     }
 }
